5b50302bfc504054d86a9ff239c512b5df9661fa
Marco Minaudo <32198621+BugNoFix@users.noreply.github.com>
2023-05-16 12:43:24 +0200

    Added user entity

---
+++ src/main/java/com/marcominaudo/gymweb/model/Role.java
@@ -0,0 +1,8 @@
+package com.marcominaudo.gymweb.model;
+
+public enum Role {
+    USER,
+    PT,
+    ADMIN
+
+}
---
+++ src/main/java/com/marcominaudo/gymweb/model/User.java
@@ -0,0 +1,103 @@
+package com.marcominaudo.gymweb.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String uuid;
+
+    @Column(unique=true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime subscriptionStart;
+
+    private LocalDateTime subscriptionEnd;
+
+    @Column(nullable = false)
+    private LocalDateTime created;
+
+    @Column(nullable = false)
+    private boolean privacy;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.created = LocalDateTime.now();
+        this.uuid = UUID.randomUUID().toString();
+        this.privacy = false;
+    }
+}
