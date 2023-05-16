e64e7544e42dd870754d61e0cd444a1fe21935f6
Marco Minaudo <32198621+BugNoFix@users.noreply.github.com>
2023-05-16 16:12:55 +0200

    Added UserBuilder

---
+++ src/main/java/com/marcominaudo/gymweb/model/builder/UserBuilder.java
@@ -0,0 +1,94 @@
+package com.marcominaudo.gymweb.model.builder;
+
+import com.marcominaudo.gymweb.model.Role;
+import com.marcominaudo.gymweb.model.User;
+
+import java.time.LocalDateTime;
+
+public class UserBuilder {
+
+    private long id;
+
+    private String uuid;
+
+    private String email;
+
+    private String name;
+
+    private String surname;
+
+    private String password;
+
+    private LocalDateTime subscriptionStart;
+
+    private LocalDateTime subscriptionEnd;
+
+    private LocalDateTime created;
+
+    private boolean privacy;
+
+    private Role role;
+
+    public UserBuilder userBuilder(){
+        return new UserBuilder();
+    }
+
+    public UserBuilder name(String name){
+        this.name = name;
+        return this;
+    }
+
+    public UserBuilder id(long id){
+        this.id = id;
+        return this;
+    }
+
+    public UserBuilder uuid(String uuid){
+        this.uuid = uuid;
+        return this;
+    }
+
+    public UserBuilder email(String email){
+        this.email = email;
+        return this;
+    }
+
+    public UserBuilder surname(String surname){
+        this.surname = surname;
+        return this;
+    }
+
+    public UserBuilder password(String password){
+        this.password = password;
+        return this;
+    }
+
+    public UserBuilder subscriptionStart(LocalDateTime subscriptionStart){
+        this.subscriptionStart = subscriptionStart;
+        return this;
+    }
+
+    public UserBuilder subscriptionEnd(LocalDateTime subscriptionEnd){
+        this.subscriptionEnd = subscriptionEnd;
+        return this;
+    }
+
+    public UserBuilder created(LocalDateTime created){
+        this.created = created;
+        return this;
+    }
+
+    public UserBuilder privacy(boolean privacy){
+        this.privacy = privacy;
+        return this;
+    }
+
+    public UserBuilder role(Role role){
+        this.role = role;
+        return this;
+    }
+
+    public User build(){
+        return new User(id, uuid, email, name, surname, password, subscriptionStart, subscriptionEnd, created, privacy, role);
+    }
+}
