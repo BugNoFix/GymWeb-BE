60635b3751fe85cca754b03125d83f76ad6eca0a
Marco Minaudo <32198621+BugNoFix@users.noreply.github.com>
2023-05-21 16:31:49 +0200

    Added bookingBuilder

---
+++ src/main/java/com/marcominaudo/gymweb/model/builder/BookingBuilder.java
@@ -0,0 +1,58 @@
+package com.marcominaudo.gymweb.model.builder;
+
+import com.marcominaudo.gymweb.model.Booking;
+import com.marcominaudo.gymweb.model.Room;
+import com.marcominaudo.gymweb.model.User;
+
+import java.time.LocalDateTime;
+
+public class BookingBuilder {
+
+    private long id;
+    private LocalDateTime subscriptionTime;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    User user;
+
+    Room room;
+
+    public BookingBuilder bookingBuilder(){
+        return new BookingBuilder();
+    }
+
+    public BookingBuilder subscriptionTime(LocalDateTime subscriptionTime){
+        this.subscriptionTime = subscriptionTime;
+        return this;
+    }
+
+    public BookingBuilder endTime(LocalDateTime endTime){
+        this.endTime = endTime;
+        return this;
+    }
+
+    public BookingBuilder startTime(LocalDateTime startTime){
+        this.startTime = startTime;
+        return this;
+    }
+    public BookingBuilder user(User user){
+        this.user = user;
+        return this;
+    }
+
+    public BookingBuilder room(Room room){
+        this.room = room;
+        return this;
+    }
+
+    public BookingBuilder id(long id){
+        this.id = id;
+        return this;
+    }
+
+    public Booking build(){
+        return new Booking(id, subscriptionTime, startTime, endTime, user, room);
+    }
+}
